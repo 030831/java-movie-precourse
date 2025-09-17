461d357fb6bf68530f91acf923965fc9f47bcf14
030831 <kdhc7777@naver.com>
2025-09-17 20:10:12 +0900

    feat(payment): implement functionality for MovieTheaterDiscountPolicy class
    Implement discount policy for movietheater
    - add movie day discount logic
    - add time-based discount logic
    - add event discount to combine movie day and time-based discount
    - add method to using point
    - add method to card discount and cash discount

--- src/main/java/payment/MovieTheaterDiscountPolicy.java
+++ src/main/java/payment/MovieTheaterDiscountPolicy.java
@@ -1,6 +1,39 @@
 package payment;
 
+import movieTheater.Movie;
 
 public class MovieTheaterDiscountPolicy  {
 
+    public int eventDiscount(int price , Movie movie) {
+        int discounted = movieDay(price , movie);
+        discounted = timeDiscount(discounted , movie);
+
+        return discounted;
+    }
+
+    public int movieDay(int price , Movie movie) {
+        if (movie.getStartTime().getDay()%10 == 0) {
+            return (int) (price*0.9);
+        }
+
+        return price;
+    }
+
+    public int timeDiscount(int price , Movie movie) {
+        if (movie.getStartTime().getHour() <= 11 || movie.getStartTime().getHour() >= 20) {
+            return Math.max(0 , price - 2000);
+        }
+        return  price;
+    }
+
+    public int usePoint(int price , int point) {
+        return Math.max(0 , price-point);
+    }
+
+    public int cardDiscount(int price) {
+        return (int) (price*0.95);
+    }
+    public int cashDiscount(int price) {
+        return (int) (price*0.98);
+    }
 }
