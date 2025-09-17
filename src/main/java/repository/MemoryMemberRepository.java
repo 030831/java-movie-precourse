f58b1dbb5db8bb2610d2bdb1f4365011c948aeaf
030831 <kdhc7777@naver.com>
2025-09-17 20:33:13 +0900

    feat(repository): implement MemoryMemberRepository
    Add method
    - save member
    - find member by id

--- src/main/java/repository/MemoryMemberRepository.java
+++ src/main/java/repository/MemoryMemberRepository.java
@@ -2,15 +2,28 @@ package repository;
 
 import member.Member;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> memberRepository = new HashMap<>();
 
     @Override
     public void save(Member member) {
 
+        if (memberRepository.containsKey(member.getId())) {
+            throw new IllegalStateException("이미 존재하는 아이디 입니다 : "  + member.getId());
+        }
+        memberRepository.put(member.getId() , member);
     }
 
     @Override
     public Member findById(Long memberId) {
-        return null;
+        return memberRepository.get(memberId);
+    }
+
+    public void clearRepository() {
+        memberRepository.clear();
     }
 }
