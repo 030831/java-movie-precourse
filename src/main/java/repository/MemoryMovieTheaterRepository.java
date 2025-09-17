a1f6d71544af403b3f945f384a6b8c37ff4a7018
030831 <kdhc7777@naver.com>
2025-09-17 20:41:21 +0900

    feat(repository): implement MemoryMovieTheaterRepository
    Add method
    - save movie
    - find movie by id
    - find movie by name
    - check validate duplicate movie id in movie repository

--- src/main/java/repository/MemoryMovieTheaterRepository.java
+++ src/main/java/repository/MemoryMovieTheaterRepository.java
@@ -1,28 +1,50 @@
 package repository;
 
-
 import movieTheater.Movie;
-import java.util.ArrayList;
+
+import java.util.*;
 
 public class MemoryMovieTheaterRepository implements MovieTheaterRepository {
 
+    private static final HashMap<Long , Movie> movieRepository = new HashMap<>();
+    private static final MemoryClosedDateRepository memoryClosedDateRepository = new MemoryClosedDateRepository();
+
+
     @Override
     public Movie save(Movie movie) {
-        return null;
+        if (!memoryClosedDateRepository.canRunMovie(movie)) {
+            throw new IllegalStateException("영화의 상영시간과 상영관의 폐관시간이 겹칩니다.\n" +
+                    movie.getStartTime() + " " +  movie.getEndTime());
+        }
+
+        movieRepository.put(movie.getId(), movie);
+        return movie;
     }
 
     @Override
     public Movie findById(Long id) {
-        return null;
+        return movieRepository.get(id);
     }
 
     @Override
     public ArrayList<Movie> findByName(String name) {
-        return null;
+        ArrayList<Movie> movieArrayList = new ArrayList<>();
+        movieRepository.forEach((key , value) -> {
+            if (value.getName().equals(name)) {
+                movieArrayList.add(findById(key));
+            }
+        });
+        return movieArrayList;
     }
 
     @Override
     public boolean validateDuplicateMovie(Movie movie) {
-        return false;
+
+        if (movieRepository.containsKey(movie.getId())) {
+            return false;
+        }
+
+        return true;
     }
+
 }
